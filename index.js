51ae17b7939b4047b616228bda073c767fee629d
daniel9046 <danielftosta@hotmail.com>
2022-11-22 12:11:02 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,7 @@
 const Client = require('./Client.js');
 const client = new Client('[TOKEN]');
 
-function run(code){
-  if(new String(code)=="[object Object]"==false&&new String(code)=="[object JSON]"==false){
-    try {
-      return '▶ ' + eval(code);
-    } catch (error) {
-      return '▶ ' + error
-    }
-  }
-  if(new String(code)=="[object Object]" || new String(code)=="[object JSON]"){
-    try {
-      return '▶ ' + JSON.stringify(eval(code));
-    } catch (error) {
-      return '▶ ' + error
-    }
-  }
-}
 
-var allowed = [
-]
 
 client.on("hi", () => {
   console.log('hi')
@@ -28,11 +10,10 @@ client.on("hi", () => {
 })
 
 client.on('a', msg => {
-  var isAllowed = (allowed.indexOf(msg.p._id) !== -1);
   var input = msg.a.split(" ").slice(1).join(" ");
-  if(msg.a.startsWith("J>")) {
+  if(msg.a.startsWith("hi_bot")) {
     if(isAllowed) {
-      client.say(run(input))
+      client.say("hi idiot.")
     }
   }
 })
