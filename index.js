080baf6e9fd7415c0aceddf907e786ef7c8f8168
daniel9046 <danielftosta@hotmail.com>
2022-11-23 22:44:23 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 const Client = require('./Client.js');
 const client = new Client('[TOKEN]');
 
+var prefix = "!"
+var room = "test/botting"
+var botName = "ExampleBot"
 client.on("hi", () => {
-  console.log('hi')
-  client.setChannel('[ROOM]')
-  client.setName('JS Bot')
+		client.setChannel(room)
+		client.setName(`${botName} [ ${prefix}help ]`)
+		console.log('Connected')
 })
 
-client.on('a', msg => {
+client.on("a", msg => {
   var input = msg.a.split(" ").slice(1).join(" ");
-  if(msg.a.startsWith("hi_bot")) {
-    client.say("hi.")
-  }
+  let cmd =  msg.a.split(' ')[0];
+	if(cmd == `${prefix}ex`) {
+		client.say(`example`)
+	}
 })
 
 client.start()
